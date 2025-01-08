7fdf3169edf27833c70c1f3439388b1941fe8227
OscarArango1 <oarangog@unal.edu.co>
2025-01-08 18:27:20 -0500

    Update Atributos Cliente

--- src/gestorAplicacion/gestionVentas/Cliente.java
+++ src/gestorAplicacion/gestionVentas/Cliente.java
@@ -1,5 +1,16 @@
-package gestorAplicacion.gestionVentas;
+package gestorAplicacion.gestionVentas; 
+
+import java.util.ArrayList;
+
+import gestorAplicacion.Enumeraciones.*;
+import gestorAplicacion.gestionObras.Actor;
 
 public class Cliente {
-    
+    long id;
+    Funcion funcion;
+    Genero generoFavorito;
+    Actor actorFavorito;
+    ArrayList<Tiquete> ultimasCompras= new ArrayList<>();
+    String correo;
+    String tipo;
 }
