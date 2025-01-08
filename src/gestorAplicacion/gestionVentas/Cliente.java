74fadce365c671508181eddd20b2eab2f83c56e4
OscarArango1 <oarangog@unal.edu.co>
2025-01-08 18:42:39 -0500

    Getter y Setters Cliente

--- src/gestorAplicacion/gestionVentas/Cliente.java
+++ src/gestorAplicacion/gestionVentas/Cliente.java
@@ -10,7 +10,51 @@ public class Cliente {
     Funcion funcion;
     Genero generoFavorito;
     Actor actorFavorito;
-    ArrayList<Tiquete> ultimasCompras= new ArrayList<>();
+    ArrayList<Tiquete> ultimasCompras = new ArrayList<>();
     String correo;
     String tipo;
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public Funcion getFuncion() {
+        return funcion;
+    }
+    public void setFuncion(Funcion funcion) {
+        this.funcion = funcion;
+    }
+    public Genero getGeneroFavorito() {
+        return generoFavorito;
+    }
+    public void setGeneroFavorito(Genero generoFavorito) {
+        this.generoFavorito = generoFavorito;
+    }
+    public Actor getActorFavorito() {
+        return actorFavorito;
+    }
+    public void setActorFavorito(Actor actorFavorito) {
+        this.actorFavorito = actorFavorito;
+    }
+    public ArrayList<Tiquete> getUltimasCompras() {
+        return ultimasCompras;
+    }
+    public void setUltimasCompras(ArrayList<Tiquete> ultimasCompras) {
+        this.ultimasCompras = ultimasCompras;
+    }
+    public String getCorreo() {
+        return correo;
+    }
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    public String getTipo() {
+        return tipo;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    } 
 }
+
