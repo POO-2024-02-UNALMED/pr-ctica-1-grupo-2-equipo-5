22029c0ddc0d1dbdff7d278ae6ec8aad925e1f88
miguelvelez0824 <mivelezb@unal.edu.co>
2025-01-12 22:23:47 -0500

    Actualizacion Empleados, Sala, y intento de empezar la funcionalidad

--- src/gestorAplicacion/gestionFinanciera/Empleado.java
+++ src/gestorAplicacion/gestionFinanciera/Empleado.java
@@ -1,49 +1,151 @@
 package gestorAplicacion.gestionFinanciera;
-
+import java.util.Random;
 import java.util.ArrayList;
 
 public class Empleado {
-    int TrabajoRealizado;
-    boolean trabajoCorrecto;
-    int metaSemanal;
-    int puntosPositivos;
-    String Ocupacion;
-    CuentaBancaria Cuenta;
-    float Deuda;
-    ArrayList<Float> trabajoNoPagado = new ArrayList<>();
-    ArrayList<Empleado> EmpleadosPorRendimiento = new ArrayList<>();
-    ArrayList<ArrayList<String>> Horario = new ArrayList<>();
+    private final int salarioSeguridad = 6500;
+    private final int salarioAseador = 20300;
+    private static ArrayList<Empleado> EmpleadosPorRendimiento = new ArrayList<>();
+    private int TrabajoRealizado;
+    private boolean trabajoCorrecto;
+    private int metaSemanal;
+    private int puntosPositivos;
+    private String Ocupacion;
+    private boolean disponible;
+    private CuentaBancaria Cuenta;
+    private float Deuda;
+    private ArrayList<Float> trabajoNoPagado = new ArrayList<>();
+    private ArrayList<ArrayList<String>> Horario = new ArrayList<>();
 
-    public boolean ifTrabajoCorrecto(){
-        return this.trabajoCorrecto;
+    //Metodo para verificar si se hizo el trabajo bien
+    public void VerificacionTrabajo(){
+        Random random = new Random();
+        double randomValue = random.nextDouble();
+        if (randomValue > 5.0){
+            this.setTrabajoCorrecto(true);
+            this.puntosPositivos += 1;
+            System.out.println("Se realizo el trabajo correctamente");
+        }
+        else{
+            this.setTrabajoCorrecto(false);
+            System.out.println("No se realizo el trabajo correctamente");
+        }
     }
-    public void setTrabajoCorrecto(){
 
+    //Calcular sueldo
+    public int calcularSueldo(){
+        if(this.Ocupacion == "Seguridad"){
+            int Sueldo = this.TrabajoRealizado * salarioSeguridad;
+            return Sueldo;
+        }
+        else{
+            int Sueldo = this.TrabajoRealizado * salarioAseador;
+            return Sueldo;
+        }
     }
-    public int getMetaSemanal(){
+    //Trabajo
+    public int getTrabajoRealizado() {
+        return TrabajoRealizado;
+    }
+
+    public void setTrabajoRealizado(int trabajoRealizado) {
+        TrabajoRealizado = trabajoRealizado;
+    }
+
+    public boolean isTrabajoCorrecto() {
+        return trabajoCorrecto;
+    }
+
+    public void setTrabajoCorrecto(boolean trabajoCorrecto) {
+        this.trabajoCorrecto = trabajoCorrecto;
+    }
+
+    //Meta
+    public int getMetaSemanal() {
         return metaSemanal;
     }
-    public void setMetaSemanal(int NewMeta){
-        this.metaSemanal = NewMeta;
-    }
-    public int getPuntosPositivos(){
-        return this.puntosPositivos;
-    }
-    public void setPuntosPositivos(int newPuntos){
-        this.puntosPositivos = newPuntos;
-    }
-    public String getOcupacion(){
-        return this.Ocupacion;
-    }
-    public void setOcupacion(String NewOcupacion){
-        this.Ocupacion = NewOcupacion;
-    }
-    public int getTrabajoRealizado(){
-        return this.TrabajoRealizado;
-    }
-    public void setTrabajoRealizado(int realizado){
-        this.TrabajoRealizado = realizado;
+
+    public void setMetaSemanal(int metaSemanal) {
+        this.metaSemanal = metaSemanal;
     }
 
+    //Puntos
+    public int getPuntosPositivos() {
+        return puntosPositivos;
+    }
 
+    public void setPuntosPositivos(int puntosPositivos) {
+        this.puntosPositivos = puntosPositivos;
+    }
+
+    //Ocupacion
+    public String getOcupacion() {
+        return Ocupacion;
+    }
+
+    public void setOcupacion(String ocupacion) {
+        Ocupacion = ocupacion;
+    }
+
+    //Disponibilidad
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    //Cuenta Bancaria
+    public CuentaBancaria getCuenta() {
+        return Cuenta;
+    }
+
+    public void setCuenta(CuentaBancaria cuenta) {
+        Cuenta = cuenta;
+    }
+
+    //Deuda
+    public float getDeuda() {
+        return Deuda;
+    }
+
+    public void setDeuda(float deuda) {
+        Deuda = deuda;
+    }
+
+    //Trabajo no Pago
+    public ArrayList<Float> getTrabajoNoPagado() {
+        return trabajoNoPagado;
+    }
+
+    public void setTrabajoNoPagado(ArrayList<Float> trabajoNoPagado) {
+        this.trabajoNoPagado = trabajoNoPagado;
+    }
+
+    //Empleados
+    public static ArrayList<Empleado> getEmpleadosPorRendimiento() {
+        return EmpleadosPorRendimiento;
+    }
+
+    public static void setEmpleadosPorRendimiento(ArrayList<Empleado> empleadosPorRendimiento) {
+        EmpleadosPorRendimiento = empleadosPorRendimiento;
+    }
+
+    //Horario
+    public ArrayList<ArrayList<String>> getHorario() {
+        return Horario;
+    }
+
+    public void setHorario(ArrayList<ArrayList<String>> horario) {
+        Horario = horario;
+    }
+
+    //Salarios
+    public int getSalarioSeguridad(){
+        return this.salarioSeguridad;
+    }
+    public int getSalarioAseador(){
+        return this.salarioAseador;
+    }
 }
--- src/gestorAplicacion/gestionVentas/Sala.java
+++ src/gestorAplicacion/gestionVentas/Sala.java
@@ -3,9 +3,13 @@ package gestorAplicacion.gestionVentas;
 import java.util.ArrayList;
 
 public class Sala {
+    private static ArrayList <Sala> salas = new ArrayList<>();
     private ArrayList <Silla> sillas = new ArrayList<>();
+    private int numeroSala;
+    private int metrosCuadrados;
     private Boolean aseado;
     private Boolean ocupado;
+    private boolean aseador;
 
 
     //SILLAS
@@ -31,9 +35,37 @@ public class Sala {
     public void setOcupado(Boolean ocupado) {
         this.ocupado = ocupado;
     }
+
+    //Metros cuadrados
+    public int getMetrosCuadrados(){
+        return this.metrosCuadrados;
+    }
+    public void setMetrosCuadrados(int newMetros){
+        this.metrosCuadrados = newMetros;
+    }
    
-    
+    //Numero de la Sala
+    public int getNumeroSala(){
+        return this.numeroSala;
+    }
+    public void setNumeroSala(int newNumero){
+        this.numeroSala = newNumero;
+    }
 
+    //Total Salas
+    public static ArrayList<Sala> getSalas(){
+        return salas;
+    }
+    public static void setSalas(ArrayList<Sala> newSalas){
+        salas = newSalas;
+    }
 
+    //Presencia Aseador
+    public boolean isAseador() {
+        return aseador;
+    }
+    public void setAseador(boolean aseador) {
+        this.aseador = aseador;
+    }
     
 }
