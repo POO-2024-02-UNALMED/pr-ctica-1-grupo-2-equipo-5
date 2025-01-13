8e6ed614d18bec0b68a8a718244afd4ce2343cb3
miguelvelez0824 <mivelezb@unal.edu.co>
2025-01-12 23:17:54 -0500

    Comente funcionalidad

--- src/uiMain/Main.java
+++ src/uiMain/Main.java
@@ -10,36 +10,36 @@ public class Main {
 
 
     //Inicio Funcionalidad 2
-        public void AsearSalas(){
-            ArrayList<Sala> totalSalas = Sala.getSalas();
-            ArrayList<Empleado> Empleados = Empleado.getEmpleadosPorRendimiento();
-            for(Sala Elemento : totalSalas){
-                if (Elemento.getAseado() == false && Elemento.isAseador() == false && Elemento.getOcupado() == false){
-                    for(Empleado Aseador : Empleados){
-                        if(Aseador.getOcupacion() == "Aseador" && Aseador.isDisponible() == true){
-                                Elemento.setAseador(true);
-                                Elemento.setOcupado(true);
-                                Aseador.setDisponible(false);
-                                break;
-                        }
-                    }
-                }
-            }
-        }
+        // public void AsearSalas(){
+        //     ArrayList<Sala> totalSalas = Sala.getSalas();
+        //     ArrayList<Empleado> Empleados = Empleado.getEmpleadosPorRendimiento();
+        //     for(Sala Elemento : totalSalas){
+        //         if (Elemento.getAseado() == false && Elemento.isAseador() == false && Elemento.getOcupado() == false){
+        //             for(Empleado Aseador : Empleados){
+        //                 if(Aseador.getOcupacion() == "Aseador" && Aseador.isDisponible() == true){
+        //                         Elemento.setAseador(true);
+        //                         Elemento.setOcupado(true);
+        //                         Aseador.setDisponible(false);
+        //                         break;
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
 
-        public String pagarNomina(){
-            String msg = "";
-            int totalNomina = 0;
-            ArrayList<Empleado> Empleados = Empleado.getEmpleadosPorRendimiento();
-            for (Empleado Elemento : Empleados){
-                totalNomina = totalNomina + Elemento.calcularSueldo();
-            }
-            if (totalNomina > Tesoreria.getCuenta().getSaldo()){
+        // public String pagarNomina(){
+        //     String msg = "";
+        //     int totalNomina = 0;
+        //     ArrayList<Empleado> Empleados = Empleado.getEmpleadosPorRendimiento();
+        //     for (Empleado Elemento : Empleados){
+        //         totalNomina = totalNomina + Elemento.calcularSueldo();
+        //     }
+        //     if (totalNomina > Tesoreria.getCuenta().getSaldo()){
                 
-            }
+        //     }
 
 
-            return msg;
-        }
+        //     return msg;
+        // }
 
 }
