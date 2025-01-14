ed349d96270ef162887895fb9add5f5dbf73c434
OscarArango1 <oarangog@unal.edu.co>
2025-01-13 23:53:32 -0500

    Modificacion clase Artista
    Modifiqué algunos atributos de la clase artista según la lógica que pienso implementar en mi funcionalidad. Igual no hay descartes totales puesto que apenas es un intento. Lo antiguo está comentado

--- src/gestorAplicacion/gestionClases/Clase.java
+++ src/gestorAplicacion/gestionClases/Clase.java
@@ -4,36 +4,45 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import gestorAplicacion.Enumeraciones.Aptitud;
 import gestorAplicacion.gestionObras.Artista;
+import gestorAplicacion.gestionVentas.Sala;
 
 public class Clase{
-    private Aptitud area;
+    //private Aptitud area; Por ahora comento esta porque no sé exactamente como usarla
     private Profesor profesor;
     private Artista alumno;
     private List<LocalDateTime[]> horario;
     private Boolean aprobada;
     private float costoMatricula;
+    private String materiaNombre; // Nombre de la materia que se imparte en la clase
+    private int nivel; /* Nivel de la clase: 1 = Introducción, 2 = Profundización, 3 = Perfeccionamiento
+                        creo que es prudente añadirlo para la funcionalidad 4*/
+    private Sala sala; // Sala asignada para la clase
 
     //Constructor
-    public Clase(Aptitud area, Profesor profesor, Artista alumno, Boolean aprobada, float costoMatricula) {
-        this.area = area;
+    /*public Clase(Aptitud area, Profesor profesor, Artista alumno, Boolean aprobada, float costoMatricula) {
+        //this.area = area; (Por ahora comentado por lo dicho anteriormente)
         this.profesor = profesor;
-        this.alumno = alumno;
+        this.alumno = alumno;               CONSTRUCTOR ANTIGUO
         this.aprobada = aprobada;
         this.costoMatricula = costoMatricula;
         this.horario = new ArrayList<>();
+    }*/
+    public Clase(Profesor profesor, Artista alumno, List<LocalDateTime[]> horario, Boolean aprobada,
+            float costoMatricula, String materiaNombre, int nivel, Sala sala) {
+        this.profesor = profesor;
+        this.alumno = alumno;
+        this.horario = new ArrayList<>();
+        this.aprobada = aprobada;
+        this.costoMatricula = costoMatricula;
+        this.materiaNombre = materiaNombre;
+        this.nivel = nivel;
+        this.sala = sala;
     }
     
     //GETTERS Y SETTERS
 
-    //Area
-    public Aptitud getArea() {
-        return area;
-    }
-    public void setArea(Aptitud area) {
-        this.area = area;
-    }
+    
 
     //Profesor
     public Profesor getProfesor() {
@@ -73,5 +82,29 @@ public class Clase{
     }
     public void setCostoMatricula(float costoMatricula) {
         this.costoMatricula = costoMatricula;
+    }
+
+    public String getMateriaNombre() {
+        return materiaNombre;
+    }
+
+    public void setMateriaNombre(String materiaNombre) {
+        this.materiaNombre = materiaNombre;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public Sala getSala() {
+        return sala;
+    }
+
+    public void setSala(Sala sala) {
+        this.sala = sala;
     }
 }
