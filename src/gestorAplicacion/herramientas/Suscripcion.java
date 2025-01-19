9485b292428e3d032868b177fac2ff2e230d0132
juanmira19 <jmirac@unal.edu.co>
2025-01-19 14:29:43 -0500

    formato precio

--- src/gestorAplicacion/herramientas/Suscripcion.java
+++ src/gestorAplicacion/herramientas/Suscripcion.java
@@ -8,10 +8,10 @@ public enum Suscripcion {
 
     public static String tiposSuscipcion(){
         String top = String.format("%30s %30s %30s ","Tipo Suscipcion","Precio","Beneficios"+"\n\n");
-        String tipo1 = String.format("%30s %30s %30s ","BASICA","0","-------"+"\n\n");
-        String tipo2 = String.format("%30s %30s %30s ","PREMIUM","11900","10% de Descuento "+"\n");
+        String tipo1 = String.format("%30s %30s %30s ","BASICA",String.format("$%,.2f",0.0),"-------"+"\n\n");
+        String tipo2 = String.format("%30s %30s %30s ","PREMIUM",String.format("$%,.2f",11900.0),"10% de Descuento "+"\n");
         String cont = String.format("%30s %30s %30s ","","","EN TODAS LAS FUNCIONES"+"\n\n");
-        String tipo3 = String.format("%30s %30s %30s ","VIP","18900","25% de Descuento"+"\n");
+        String tipo3 = String.format("%30s %30s %30s ","VIP",String.format("$%,.2f",18900.0),"25% de Descuento"+"\n");
         String cont2 = String.format("%30s %30s %30s ","","","EN TODAS LAS FUNCIONES");
 
         
