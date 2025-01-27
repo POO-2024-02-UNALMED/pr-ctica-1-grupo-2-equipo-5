bc86661026e3b5f17a247b32ec6bb3b95bd7c38c
frncscp <franciscocerenporto@gmail.com>
2025-01-26 21:14:41 -0500

    remover static en objeto serializable

--- src/baseDatos/Teatro.java
+++ src/baseDatos/Teatro.java
@@ -8,35 +8,35 @@ import gestorAplicacion.gestionObras.Actor;
 
 public class Teatro implements Serializable{
     
-    public static final long serialVersionUID = 1L;
-    public static Teatro instancia;
-    static Tesoreria tesoreria = new Tesoreria(0, 100);
+    private final long serialVersionUID = 1L;
+    private static Teatro instancia;
+    private Tesoreria tesoreria = new Tesoreria(0, 100);
 
-    public static Sala sala1 = new Sala(1, 100, 24);
+    private Sala sala1 = new Sala(1, 100, 24);
 
     //empleados
-    public static ArrayList<Empleado> empleadosPorRendimiento = new ArrayList<>();
-    public static ArrayList<Empleado> tipoSeguridad = new ArrayList<>();
-    public static ArrayList<Empleado> tipoAseador = new ArrayList<>();
-    public static ArrayList<Empleado> tipoProfesor = new ArrayList<>();
+    private ArrayList<Empleado> empleadosPorRendimiento = new ArrayList<>();
+    private ArrayList<Empleado> tipoSeguridad = new ArrayList<>();
+    private ArrayList<Empleado> tipoAseador = new ArrayList<>();
+    private ArrayList<Empleado> tipoProfesor = new ArrayList<>();
     
     //artistas
-    public static List<Actor> actores = new ArrayList<Actor>();
-    public static List<Director> directors = new ArrayList<Director>(); //lista que almacenará todos los actores creados
+    private List<Actor> actores = new ArrayList<Actor>();
+    private List<Director> directors = new ArrayList<Director>(); //lista que almacenará todos los actores creados
     
     //obras
-    public static ArrayList<Obra> obras = new ArrayList<>();
-    public static ArrayList<Obra> estadoCriticoS;
+    private ArrayList<Obra> obras = new ArrayList<>();
+    private ArrayList<Obra> estadoCriticoS;
 
     //clientes
-    public static ArrayList<Cliente> clientes = new ArrayList<>();
+    private ArrayList<Cliente> clientes = new ArrayList<>();
     
     //funciones
-    public static ArrayList <Funcion> funcionesCreadas= new ArrayList<>();
+    private ArrayList <Funcion> funcionesCreadas= new ArrayList<>();
     //salas
-    public static ArrayList <Sala> salas = new ArrayList<>();
+    private ArrayList <Sala> salas = new ArrayList<>();
 
     //tiquetes
-    private static ArrayList <Tiquete> tiquetes=new ArrayList<>();
+    private ArrayList <Tiquete> tiquetes=new ArrayList<>();
 
 }
